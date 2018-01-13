28fd1bb0c107a8a7214c264dc3e056c6c9b9d0d3
Thang Dinh <thangdv@ttgroup.com.vn>
2018-01-13 09:50:57 +0700

    commit change

--- Tvita_Test/Scripts/tvita/community.js
+++ Tvita_Test/Scripts/tvita/community.js
@@ -1,1 +1,20 @@
-﻿
+﻿$(function () {
+    $.ajax({
+        type: "GET",
+        dataType: 'json',
+        url: Config.AppUrl + "/CommunityDevelopment/GetRelatedCommunity/",
+        contentType: 'application/json',
+        data: { id: $('#idCommunity').val() },
+        success: function (resp) {
+            if (resp.data.length != 0) {
+                $.each(resp.data, function (k, v) {
+                    var div = $('<div class="col-sm-4 col-md-4 col-lg-4"><div class="n-item"><div class="n-cover"><img src="../../Content/images/photos/transparent-388x388.png" style="background-image: Url(\'' + Config.AppUrl + '/Content/pictures/' + v.Post_Pic_URL + '\')">' + '</div><div class="n-content">' + v.Post_Name + '</div><div class="n-detail-content" data-maxrow="5">' + v.Post_Description + '</div><div class="n-view-more"><a href="' + Config.AppUrl + "/News/Details/" + v.Post_ID + '">' + $.i18n("view_more") + '</a></div></div></div>');
+                    $('#relatedCommunity').append(div)
+                });
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+
+        }
+    });
+})
